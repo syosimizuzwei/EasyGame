333fac4c27af96a668359ab05c5a5dc2eacef0c9
Syotaro Shimizu <syotaroshimizu@Syotaro-no-MacBook-Pro.local>
2014-12-24 10:32:42 +0900

    Initial Commit

---
+++ easy game/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  easy game
+//
+//  Created by Syotaro Shimizu on 2014/12/24.
+//  Copyright (c) 2014年 Syotaro Shimizu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
