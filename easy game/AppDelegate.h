e3d9f79e196393fd21591aa73db341fcd633f3ff
Shotaro <futatumeno@gmail.com>
2015-05-16 21:10:40 +0900

    微妙

--- easy game/AppDelegate.h
+++ easy game/AppDelegate.h
@@ -6,12 +6,21 @@
 //  Copyright (c) 2014年 Syotaro Shimizu. All rights reserved.
 //
 
+//#import <UIKit/UIKit.h>
+//
+//@interface AppDelegate : UIResponder <UIApplicationDelegate>
+//
+//@property (strong, nonatomic) UIWindow *window;
+//
+//
+//@end
 #import <UIKit/UIKit.h>
+@class ViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (retain, nonatomic) ViewController *viewController;
 
 @end
 
